5fc05bf3737e4643d052880f4c6d78ac62582d65
JANAVI SINGH <janavisingh1013@gmail.com>
2025-11-02 22:57:00 +0530

    Add files via upload

---
+++ Program to compare 2 numbers entered by the users and display which is greater, smaller or equal.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main (void)
+{
+ int a,b
+ printf("enter first numbers");
+ scanC:(:%d,&a);
+ printf("enter second numbrt");
+ scanf("%d,&b");
+ printf("%d is greater then %d\n",a,b);
+ if (a>b)
+        else if (a<b)
+                 printf ("%d is less then %d\n",a,b);
+ else
+    printf("both numbers are equal \n");
+return ;
+}
